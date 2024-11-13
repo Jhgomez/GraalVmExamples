89713cf08df85369f53c52791498d557d63d23e0
Juan Gomez <juan.e.g.f@outlook.com>
2024-11-13 09:29:16 -0600

    chore: add graalpy dependencies

--- GraalPy/openai/build.gradle.kts
+++ GraalPy/openai/build.gradle.kts
@@ -12,6 +12,9 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    implementation("org.graalvm.polyglot:polyglot:24.1.1")
+    implementation("org.graalvm.polyglot:python:24.1.1")
 }
 
 tasks.test {
