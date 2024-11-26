5743e8dd41080bf641079d7be895a77362c28c1f
Juan Gomez <juan.e.g.f@outlook.com>
2024-11-26 17:58:20 -0600

    chore: add save image method and add python code

--- GraalPy/jbang/Main.java
+++ GraalPy/jbang/Main.java
@@ -1,5 +1,6 @@
 //DEPS org.graalvm.python:python-language:24.1.1
 //DEPS org.graalvm.python:python-resources:24.1.1
+//DEPS org.graalvm.python:python-launcher:24.1.1
 //DEPS org.graalvm.python:python-embedding:24.1.1
 //DEPS org.graalvm.python:python-embedding-tools:24.1.1
 //PIP qrcode==8.0
@@ -9,29 +10,29 @@ import org.graalvm.python.embedding.utils.GraalPyResources;
 public class Main {
     public static void main(String[] args) {
         String qrCodeInfo;
-//        if (args[0].length > 1) {
-//            throw new RuntimeException("Please provide no more or less than one command line argument");
-//        } else {
-//            qrCodeInfo = args[0];
-//        }
+        if (args.length == 1) {
+            qrCodeInfo = args[0];
+        } else {
+            throw new RuntimeException("Please provide no more or less than one command line argument");
+        }
 
         try (var context = GraalPyResources.contextBuilder().option("python.PythonHome", "").build()) {
-//            MakeQrCodeFunction qrCodeFunction = context.eval("python", """
-//                        import qrcode
-//                        # from qrcode.image.pure import PyPNGImage
-//                        from qrcode.image.svg import SvgPathFillImage
-//
-//                        def make_qrCode(text_info):
-//                                return qrcode.make(text_info, image_factory=SvgPathFillImage)
-//
-//                        make_qrCode
-//                    """).as(MakeQrCodeFunction.class);
+            MakeQrCodeFunction qrCodeFunction = context.eval("python",
+                    // languages=python
+                    """
+                        import qrcode
+                        # from qrcode.image.pure import PyPNGImage
+                        from qrcode.image.svg import SvgPathFillImage
+    
+                        def make_qrcode(text_info):
+                                return qrcode.make(text_info, image_factory=SvgPathFillImage)
+    
+                        make_qrcode
+                        """).as(MakeQrCodeFunction.class);
 
-//            SvgImage image = qrCodeFunction.apply("https://github.com/graalvm/graal-languages-demos/blob/main/graalpy/graalpy-jbang-qrcode/qrcode.java");
-//
-//            System.out.print(image.toString());
-        } catch (Exception e) {
-            // TODO: handle exception
+            SvgImage image = qrCodeFunction.apply(qrCodeInfo);
+
+            image.save("qrCode.svg");
         }
     }
 
@@ -42,6 +43,6 @@ public class Main {
     }
 
     public interface SvgImage {
-        String toString();
+        void save(String path);
     }
 }
