e5b391380111f8a24eaaaa1d5d57d806d2a0817a
Juan Gomez <juan.e.g.f@outlook.com>
2024-12-01 12:03:09 -0600

    chore: add controller

---
+++ GraalPy/pygal/pygal_maven/src/main/java/okik/tech/springpygal/PygalController.java
@@ -0,0 +1,20 @@
+package okik.tech.springpygal;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PygalController {
+    PygalServicePurePython purePythonService;
+
+    public PygalController(
+            PygalServicePurePython purePythonService
+    ) {
+        this.purePythonService = purePythonService;
+    }
+
+    @GetMapping("/purePython")
+    public String purePython() {
+        return purePythonService.renderChart();
+    }
+}
