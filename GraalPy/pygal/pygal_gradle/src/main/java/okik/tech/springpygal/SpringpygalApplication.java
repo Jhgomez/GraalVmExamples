4c092b4031f4206a332beaa3fc0d2bb206bfc800
Juan Gomez <juan.e.g.f@outlook.com>
2024-11-30 19:53:28 -0600

    chore: add galpy gradle initial project

---
+++ GraalPy/pygal/pygal_gradle/src/main/java/okik/tech/springpygal/SpringpygalApplication.java
@@ -0,0 +1,13 @@
+package okik.tech.springpygal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringpygalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringpygalApplication.class, args);
+	}
+
+}
