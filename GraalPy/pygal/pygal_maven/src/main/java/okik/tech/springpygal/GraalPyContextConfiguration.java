694efacb7d7f55e7c8a692231095ffe561fc63f7
Juan Gomez <juan.e.g.f@outlook.com>
2024-12-01 11:37:59 -0600

    chore: add graalpy context config

---
+++ GraalPy/pygal/pygal_maven/src/main/java/okik/tech/springpygal/GraalPyContextConfiguration.java
@@ -0,0 +1,28 @@
+package okik.tech.springpygal;
+
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Value;
+import org.graalvm.python.embedding.utils.GraalPyResources;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GraalPyContextConfiguration {
+
+    @Bean(destroyMethod = "close")
+    GraalPyContext graalPyContext() {
+        Context context = GraalPyResources.createContext();
+        context.initialize("python");
+        return new GraalPyContext(context);
+    }
+
+    public record GraalPyContext(Context context) {
+        public Value eval(String source) {
+            return context.eval("python", source);
+        }
+
+        public void close() {
+            context.close(true);
+        }
+    }
+}
