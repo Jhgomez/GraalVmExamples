5cf217171925d5c8f6819e5efff96ae925004af2
Juan Gomez <juan.e.g.f@outlook.com>
2024-11-30 22:18:24 -0600

    chore: add configuration file to inject polyglot python context

---
+++ GraalPy/pygal/pygal_gradle/src/main/java/okik/tech/springpygal/GraalPyContextConfiguration.java
@@ -0,0 +1,29 @@
+package okik.tech.springpygal;
+
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Value;
+import org.graalvm.python.embedding.utils.GraalPyResources;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GraalPyContextConfiguration {
+
+    @Bean(destroyMethod = "close")
+    public GraalPyContext graalPyContext() {
+        Context context = GraalPyResources.createContext();
+        context.initialize("python");
+
+        return new GraalPyContext(context);
+    }
+
+    record GraalPyContext(Context context) {
+        public Value eval(String sourceCode) {
+            return context.eval("ptyhon", sourceCode);
+        }
+
+        public void close() {
+            context.close();
+        }
+    }
+}
