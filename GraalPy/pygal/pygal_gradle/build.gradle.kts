39fcfa3899f7f4c9fb276af2013206dfb73d2452
Juan Gomez <juan.e.g.f@outlook.com>
2024-11-30 20:21:52 -0600

    chore: add graalpy dependencies

--- GraalPy/pygal/pygal_gradle/build.gradle.kts
+++ GraalPy/pygal/pygal_gradle/build.gradle.kts
@@ -1,8 +1,11 @@
+val graalPyVersion = "24.1.1"
+
 plugins {
 	java
 	id("org.springframework.boot") version "3.4.0"
 	id("io.spring.dependency-management") version "1.1.6"
 	id("org.graalvm.buildtools.native") version "0.10.3"
+	id("org.graalvm.python") version "24.1.1"
 }
 
 group = "okik.tech"
@@ -20,6 +23,9 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.graalvm.polyglot:polyglot:$graalPyVersion")
+	implementation("org.graalvm.polyglot:python:$graalPyVersion")
+	implementation("org.graalvm.python:python-embedding:$graalPyVersion")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
