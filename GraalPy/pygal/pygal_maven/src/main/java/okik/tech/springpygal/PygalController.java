a5172c9047a76f49a804a004eba5720eef3c71bc
Juan Gomez <juan.e.g.f@outlook.com>
2024-12-02 08:12:40 -0600

    chore: add mixed service to controller

--- GraalPy/pygal/pygal_maven/src/main/java/okik/tech/springpygal/PygalController.java
+++ GraalPy/pygal/pygal_maven/src/main/java/okik/tech/springpygal/PygalController.java
@@ -1,20 +1,31 @@
 package okik.tech.springpygal;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @RestController
 public class PygalController {
     PygalServicePurePython purePythonService;
+    PygalServiceMixed mixedService;
 
     public PygalController(
-            PygalServicePurePython purePythonService
+            PygalServicePurePython purePythonService,
+            PygalServiceMixed mixedService
     ) {
         this.purePythonService = purePythonService;
+        this.mixedService = mixedService;
     }
 
     @GetMapping("/purepython")
     public String purePython() {
         return purePythonService.renderChart();
+    }
+
+    @RequestMapping(method = GET, path = "/mixed")
+    String mixed() {
+        return mixedService.renderChart();
     }
 }
