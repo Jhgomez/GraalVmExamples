e11bda1359d3c02db5aaa27b9f6dc81fb0bbf527
Juan Gomez <juan.e.g.f@outlook.com>
2024-12-02 07:53:08 -0600

    chore: add python code

--- GraalPy/pygal/pygal_gradle/src/main/java/okik/tech/springpygal/PygalServiceMixed.java
+++ GraalPy/pygal/pygal_gradle/src/main/java/okik/tech/springpygal/PygalServiceMixed.java
@@ -11,6 +11,23 @@ public class PygalServiceMixed implements PygalService {
     private final String pythonGeneratedSvg;
 
     public PygalServiceMixed(GraalPyContext context) {
+        ;
+
+
+        Render render = context.eval(
+                // language=python
+                """
+                    import pygal
+                    
+                    def render_xy_chart(values):
+                        xy_chart = pygal.XY()
+                        xy_chart.title = "XY with GraalPy"
+                        for value in values:
+                            xy_chart.add(value.tag(), value.values())
+                        return xy_chart.render().decode()
+                    
+                    render_xy_chart
+                    """).as(Render.class);
 
     }
 
