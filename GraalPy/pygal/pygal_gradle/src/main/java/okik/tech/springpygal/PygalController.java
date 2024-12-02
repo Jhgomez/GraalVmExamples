f93a0f46987e81ba4d199395f0e03a73bdc9f95a
Juan Gomez <juan.e.g.f@outlook.com>
2024-12-02 08:00:57 -0600

    chore: register mixed endpoint

--- GraalPy/pygal/pygal_gradle/src/main/java/okik/tech/springpygal/PygalController.java
+++ GraalPy/pygal/pygal_gradle/src/main/java/okik/tech/springpygal/PygalController.java
@@ -1,20 +1,31 @@
 package okik.tech.springpygal;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @RestController
 public class PygalController {
     private final PygalServicePurePython servicePurePython;
+    private final PygalServiceMixed serviceMixed;
 
     public PygalController(
-            PygalServicePurePython servicePurePython
+            PygalServicePurePython servicePurePython,
+            PygalServiceMixed serviceMixed
     ) {
         this.servicePurePython = servicePurePython;
+        this.serviceMixed = serviceMixed;
     }
 
     @GetMapping("/purepython")
     String purePython() {
         return servicePurePython.render();
+    }
+
+    @RequestMapping(method = GET, path = "/mixed")
+    String mixed() {
+        return serviceMixed.render();
     }
 }
