66bf4994da3ba27254dacda7ad120945020ed9d2
Juan Gomez <juan.e.g.f@outlook.com>
2024-12-05 16:14:46 -0600

    chore: change param type

--- GraalPy/javafxWithScripting/Maven/src/main/java/okik/tech/customControls/observers/Observer.java
+++ GraalPy/javafxWithScripting/Maven/src/main/java/okik/tech/customControls/observers/Observer.java
@@ -1,7 +1,5 @@
 package okik.tech.customControls.observers;
 
-import java.util.function.Consumer;
-
 public interface Observer {
-    <T> void onChange(Consumer<T> onChange);
+    <T> void update(T onChange);
 }
