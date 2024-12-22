7f2f9269559384df2b23137d7d137e21542d1aba
Juan Gomez <juan.e.g.f@outlook.com>
2024-12-22 11:03:41 -0600

    chore: add jython, allow host access and set up animation function from python

--- GraalPy/javafxWithScripting/Maven/src/main/java/okik/tech/App.java
+++ GraalPy/javafxWithScripting/Maven/src/main/java/okik/tech/App.java
@@ -10,8 +10,12 @@ import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.util.Duration;
-import java.util.concurrent.atomic.AtomicInteger;
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.HostAccess;
+import org.graalvm.polyglot.Value;
+import org.graalvm.python.embedding.utils.GraalPyResources;
 
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class App extends Application {
 
@@ -26,7 +30,7 @@ public class App extends Application {
                 - `dir(books.getLast())`
                 - `add_book("29874", "Design Patterns", "Eric Freeman & Elizabeth Freeman", "educational", 1)`
                 - `sys.exit(0)` or `os.listdir()`
-                - `stage`
+                - `window`
                 - `animate()` or `move()`
                 
                 """;
@@ -42,10 +46,86 @@ public class App extends Application {
 
         var scene = new Scene(vBox, 640, 420);
 
+        stage.setTitle("GraalPy Console");
+        stage.setScene(scene);
+        stage.show();
+
+        Context context = GraalPyResources
+                .contextBuilder()
+                .allowHostAccess(HostAccess.ALL)
+                .allowAllAccess(true)           // https://github.com/graalvm/graal-languages-demos/blob/main/graalpy/graalpy-jython-guide/src/main/java/org/example/GraalPyInputCallback.java
+                .option("python.EmulateJython", "true")
+                .build();
+
+        context.initialize("python");
+        Value pythonBindings = context.getBindings("python");
+        pythonBindings.putMember("Book", Book.class);
+        pythonBindings.putMember("books", BookRepository.books);
+        pythonBindings.putMember("stage", stage);
+
+        // imports in jython https://github.com/oracle/graalpython/blob/master/docs/user/Python-on-JVM.md
+        // imports alternative https://github.com/oracle/graalpython/blob/master/docs/user/Interoperability.md
+        // debuggin https://github.com/oracle/graalpython/blob/master/docs/user/Tooling.md
+        context.eval("python",
+                // language=python
+                """
+                        import sys, os, time, math
+                        import java.util.concurrent.atomic.AtomicInteger as AtomicInteger
+                        import javafx.animation.Timeline as Timeline
+                        import javafx.animation.KeyFrame as KeyFrame
+                        import javafx.util.Duration as Duration
+                        
+                        x = 0
+                        y = 0
+
+                        def add_book(isbn, name, author, genre, ranking):
+                            book = Book(isbn, name, author, genre, ranking)
+                            books.add(book)
+                            return book
+                        
+                        def animate():
+                            atomic = AtomicInteger()
+                            global x
+                            x = stage.getX()
+                            global y
+                            y = stage.getY()
+                            timeline = Timeline(KeyFrame(Duration.millis(40), lambda a:_animateCallback(atomic)))
+                            timeline.setCycleCount(50)
+                            timeline.play()
+                        
+                        def _animateCallback(c):
+                            c = c.getAndIncrement()
+                            if (c == 49):
+                                stage.setX(x)
+                                stage.setY(y)
+                            else:
+                                vX = x + 50 * math.cos(c / 2.0)
+                                vY = y + 50 * math.sin(c / 2.0)
+    
+                                stage.setX(vX)
+                                stage.setY(vY)
+                        """);
+
         inputArea.setOnKeyPressed((event) -> {
             if (event.getCode() == KeyCode.ENTER) {
                 console.appendText("> ");
                 console.appendText(inputArea.getText());
+
+                try {
+                    Value result = context.eval("python", inputArea.getText());
+
+                    if (result.isString()) {
+                        console.appendText(result.asString());
+                    } else {
+                        console.appendText(result.toString());
+                    }
+
+                    console.appendText("\n");
+
+                } catch (Exception e) {
+
+                    console.appendText(String.format("Syntax error: %1$s %2$s %3$s %4$s\n", e.fillInStackTrace(), e.getStackTrace(), e.getCause(), e.getSuppressed()));
+                }
 
                 inputArea.clear();
 
@@ -54,26 +134,7 @@ public class App extends Application {
             }
         });
 
-//        Context context = GraalPyResources.contextBuilder().allowHostAccess(HostAccess.ALL).build();
-//        context.initialize("python");
-//        Value pythonBindings = context.getBindings("python");
-//        pythonBindings.putMember("book", Book.class);
-//        pythonBindings.putMember("books", BookRepository.books);
-//        pythonBindings.putMember("window", stage);
-//
-//        context.eval("python",
-//                // language=python
-//                """
-//                        import sys, os, time, math
-//
-//                        def add_book(isbn, name, author, genre, ranking):
-//                            book = Book(isbn, name, author, genre, ranking)
-//                            return book
-//                        """);
 
-        stage.setTitle("GraalPy Console");
-        stage.setScene(scene);
-        stage.show();
 
     }
 
